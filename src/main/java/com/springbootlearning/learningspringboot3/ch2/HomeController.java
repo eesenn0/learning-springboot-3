2877c811102c909cfa4c4b8ed0132f3d8ffd7375
eesenn0 <emreesen7@gmail.com>
2023-08-02 16:50:13 +0300

    Video migrated to its own class

--- src/main/java/com/springbootlearning/learningspringboot3/ch2/HomeController.java
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/HomeController.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
- 
-   record Video(String name) {
-
-    }
-
+    
     List<Video> videos = List.of(new Video("Need HELP with your SPRING BOOT 3 App?"), 
       new Video("Don't do THIS to your own CODE!"), new Video("SECRETS to fix BROKEN CODE!"));
 
