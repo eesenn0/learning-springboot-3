16eff1147eed5657b3fab82698b6ff520e802ddb
eesenn0 <emreesen7@gmail.com>
2023-08-04 16:23:29 +0300

    Created VideoEntity class

---
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/VideoEntity.java
@@ -0,0 +1,50 @@
+package com.springbootlearning.learningspringboot3.ch2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class VideoEntity {
+    
+    // @Id - JPA's annotation to flag the primary key
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String description;
+
+    protected VideoEntity() {
+        
+    }
+
+    public VideoEntity(String name, String description) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
