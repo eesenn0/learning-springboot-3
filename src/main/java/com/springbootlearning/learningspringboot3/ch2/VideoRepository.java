e07ebf6c58e5f7b031936953a987effd0f31e110
eesenn0 <emreesen7@gmail.com>
2023-08-04 18:45:45 +0300

    made an form for /multi-field-search post method via mustache

--- src/main/java/com/springbootlearning/learningspringboot3/ch2/VideoRepository.java
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/VideoRepository.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.learningspringboot3.ch2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface VideoRepository extends JpaRepository <VideoEntity, Long> {
-
+    
+    List<VideoEntity> findByName(String name);
 }
