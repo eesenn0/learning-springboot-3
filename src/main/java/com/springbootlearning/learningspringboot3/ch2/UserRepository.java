1fec5c219b11a3e6365d2f83dfac46972fc99666
eesenn0 <emreesen7@gmail.com>
2023-08-05 17:13:54 +0300

    corrected the extended class

--- src/main/java/com/springbootlearning/learningspringboot3/ch2/UserRepository.java
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/UserRepository.java
@@ -1,8 +1,7 @@
 package com.springbootlearning.learningspringboot3.ch2;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface UserRepository extends JpaRepository<UserAccount, Long> {
-    
-    UserAccount findByUsername(String username);
+public interface UserRepository extends Repository<UserAccount, Long> {
+	UserAccount findByUsername(String username);
 }
