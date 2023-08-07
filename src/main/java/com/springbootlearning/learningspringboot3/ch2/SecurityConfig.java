8d653970cbcccfeb9a24bb25ed2d14ba5c2f50d4
eesenn0 <emreesen7@gmail.com>
2023-08-07 23:28:00 +0300

    add the configureSecurity on SecurityConfig.java

--- src/main/java/com/springbootlearning/learningspringboot3/ch2/SecurityConfig.java
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/SecurityConfig.java
@@ -1,35 +1,41 @@
 package com.springbootlearning.learningspringboot3.ch2;
 
+import java.util.stream.Stream;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        
-        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build());
 
         userDetailsManager.createUser(User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("password")
-            .roles("ADMIN")
-            .build());
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build());
+
+        userDetailsManager.createUser(User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build());
 
         return userDetailsManager;
     }
-    
+
     @Bean
     CommandLineRunner initUsers(UserManagementRepository repository) {
         return args -> {
@@ -41,6 +47,22 @@ public class SecurityConfig {
     @Bean
     UserDetailsService userService(UserRepository repo) {
         return username -> repo.findByUsername(username)
-            .asUser();
+                .asUser();
+    }
+
+    @Bean
+    SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests() //
+                .requestMatchers("/login").permitAll() //
+                .requestMatchers("/", "/search").authenticated() //
+                .requestMatchers(HttpMethod.GET, "/api/**").authenticated() //
+                .requestMatchers("/admin").hasRole("ADMIN") //
+                .requestMatchers(HttpMethod.POST, "/new-video", "/api/**").hasRole("ADMIN") //
+                .anyRequest().denyAll() //
+                .and() //
+                .formLogin() //
+                .and() //
+                .httpBasic();
+        return http.build();
     }
 }
