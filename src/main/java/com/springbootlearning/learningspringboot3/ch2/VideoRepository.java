fdf2a4b568796b229f740ae2ad9202a597625869
eesenn0 <emreesen7@gmail.com>
2023-08-10 18:05:55 +0300

    give permission to delete if the username and the authentication name is the same

--- src/main/java/com/springbootlearning/learningspringboot3/ch2/VideoRepository.java
+++ src/main/java/com/springbootlearning/learningspringboot3/ch2/VideoRepository.java
@@ -1,6 +1,8 @@
 package com.springbootlearning.learningspringboot3.ch2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import java.util.List;
 
 public interface VideoRepository extends JpaRepository <VideoEntity, Long> {
@@ -11,4 +13,8 @@ public interface VideoRepository extends JpaRepository <VideoEntity, Long> {
 
     List<VideoEntity> findByNameContainsOrDescriptionContainsAllIgnoreCase(String partialName, 
         String partialDescription);
+
+    @PreAuthorize("#entity.username == authentication.name")
+    @Override
+    void delete(VideoEntity entity);
 }
