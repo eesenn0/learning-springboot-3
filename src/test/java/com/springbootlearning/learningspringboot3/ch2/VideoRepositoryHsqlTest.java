5f19b18899e087519591283c8a0d9f206704622d
eesenn0 <emreesen7@gmail.com>
2023-08-19 00:13:26 +0300

    created 'VideoRepositoryHsqlTest' class

---
+++ src/test/java/com/springbootlearning/learningspringboot3/ch2/VideoRepositoryHsqlTest.java
@@ -0,0 +1,32 @@
+package com.springbootlearning.learningspringboot3.ch2;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class VideoRepositoryHsqlTest {
+    
+    @Autowired
+    VideoRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        repository.saveAll(
+            List.of(
+                new VideoEntity(
+                    "alice", 
+                    "Need HELP with your SPRING BOOT 3 App?", 
+                    "SPRING BOOT 3 will only speed things up."),
+                new VideoEntity(
+                    "alice",
+                    "Don't do THIS to your own CODE!",
+                    "As a pro developer, never ever EVER do this to your code."),
+                new VideoEntity(
+                    "bob",
+                    "SECRETS to fix BROKEN CODE!",
+                    "Discover ways to not only debug your code")));
+    }
+}
