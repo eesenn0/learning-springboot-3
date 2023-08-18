8c763c8405f1c7f10d1fc1e295889aebc201ae83
eesenn0 <emreesen7@gmail.com>
2023-08-19 00:26:15 +0300

    added find all and find by name test methods

--- src/test/java/com/springbootlearning/learningspringboot3/ch2/VideoRepositoryHsqlTest.java
+++ src/test/java/com/springbootlearning/learningspringboot3/ch2/VideoRepositoryHsqlTest.java
@@ -2,7 +2,10 @@ package com.springbootlearning.learningspringboot3.ch2;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -28,5 +31,23 @@ public class VideoRepositoryHsqlTest {
                     "bob",
                     "SECRETS to fix BROKEN CODE!",
                     "Discover ways to not only debug your code")));
+    }
+
+    @Test
+    void findAllShouldProduceAllVideos() {
+        List<VideoEntity> videos = repository.findAll();
+
+        assertThat(videos).hasSize(3);
+    }
+
+    @Test
+    void findByNameShouldRetrieveOneEntry() {
+        List<VideoEntity> videos = repository
+            .findByNameContainsIgnoreCase("SpRinG bOOt 3");
+
+        assertThat(videos).hasSize(1);
+        assertThat(videos).extracting(VideoEntity::getName)
+            .containsExactlyInAnyOrder(
+                "Need HELP with your SPRING BOOT 3 App?");
     }
 }
